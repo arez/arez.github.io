e73be30ee42e487f8fb72fba2a5c8bb74412c925
Peter Donald <peter@realityforge.org>
2026-04-09 18:06:09 +1000

    Publish website

--- persist/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"arez.persist"},{"l":"arez.persist.processor"},{"l":"arez.persist.runtime"},{"l":"arez.persist.runtime.browser"}];updateSearchResults();
